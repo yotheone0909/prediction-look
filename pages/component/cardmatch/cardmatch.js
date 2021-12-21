631a89af38269b3c3a2d82e4692f5919a92ece46
Yothin Homjan <yothin.ho@bitnance.com>
2021-12-21 18:07:25 +0700

    Sow layout

--- pages/component/cardmatch/cardmatch.js
+++ pages/component/cardmatch/cardmatch.js
@@ -1,40 +1,44 @@
-export default function CardMatch() {
+export default function CardMatch({ sport }) {
     return (
-        <div>
-            <div class="max-w-sm rounded overflow-hidden shadow-lg mt-16 mx-8">
-                <img class="w-full" src="https://c4.wallpaperflare.com/wallpaper/500/442/354/outrun-vaporwave-hd-wallpaper-preview.jpg" alt="Sunset in the mountains" />
-                <div class="px-6 py-4">
-                    <div class="font-bold text-xl mb-2">Arsenal vs Sunderland</div>
+        <div className="flex-1 basis-1/4">
+            <div className="max-w-sm rounded overflow-hidden shadow-lg m-4">
+                <img className="w-full" src="https://c4.wallpaperflare.com/wallpaper/500/442/354/outrun-vaporwave-hd-wallpaper-preview.jpg" alt="Sunset in the mountains" />
+                <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">{sport.matchName}</div>
 
-                    <div class="flex justify-between mb-1">
-                        <span class="text-base font-medium text-blue-700">Home</span>
-                        <span class="text-sm font-medium text-blue-700 ">15%</span>
-                    </div>
-                    <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-                        <div class="bg-blue-600 h-2.5 rounded-full" style={{ width: '15%' }}></div>
+                    <div className="flex">
+                        <p>{sport.timeLock}Close Prediction : 10D 5H 20M 10S</p>
                     </div>
 
-                    <div class="flex justify-between mb-1">
-                        <span class="text-base font-medium text-blue-700">Draw</span>
-                        <span class="text-sm font-medium text-blue-700">65%</span>
+                    <div className="flex justify-between mb-1">
+                        <span className="text-base font-medium text-blue-700">Home</span>
+                        <span className="text-sm font-medium text-blue-700 ">{sport.homePercentage}</span>
                     </div>
-                    <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-                        <div class="bg-orange-500 h-2.5 rounded-full" style={{ width: '65%' }}></div>
+                    <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                        <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: sport.homePercentage }}></div>
                     </div>
 
-                    <div class="flex justify-between mb-1">
-                        <span class="text-base font-medium text-blue-700 ">Away</span>
-                        <span class="text-sm font-medium text-blue-700 ">20%</span>
+                    <div className="flex justify-between mb-1">
+                        <span className="text-base font-medium text-blue-700">Draw</span>
+                        <span className="text-sm font-medium text-blue-700">{sport.drawPercentage}</span>
                     </div>
-                    <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-                        <div class="bg-red-600 h-2.5 rounded-full" style={{ width: '20%' }}></div>
+                    <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                        <div className="bg-orange-500 h-2.5 rounded-full" style={{ width: sport.drawPercentage }}></div>
+                    </div>
+
+                    <div className="flex justify-between mb-1">
+                        <span className="text-base font-medium text-blue-700 ">Away</span>
+                        <span className="text-sm font-medium text-blue-700 ">{sport.awayPercentage}</span>
+                    </div>
+                    <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                        <div className="bg-red-600 h-2.5 rounded-full" style={{ width: sport.awayPercentage }}></div>
                     </div>
 
                 </div>
-                <div class="px-2 pt-4 pb-2 flex ">
-                    <button class="bg-blue-500 btn-sm mr-1 hover:bg-blue-700 text-white font-bold py-2 rounded-full w-1/3 mb-4">Home</button>
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white mr-1 font-bold py-2 rounded-full w-1/3 mb-4 ">Draw</button>
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white mr-1 font-bold py-2 rounded-full w-1/3 mb-4">Away</button>
+                <div className="px-2 pt-4 pb-2 flex ">
+                    <button className="bg-blue-500 btn-sm mr-1 hover:bg-blue-700 text-white font-bold py-2 rounded-full w-1/3 mb-4">Home</button>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white mr-1 font-bold py-2 rounded-full w-1/3 mb-4 ">Draw</button>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white mr-1 font-bold py-2 rounded-full w-1/3 mb-4">Away</button>
                 </div>
             </div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,89 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import CardMatch from './component/cardmatch/cardmatch'
+import CardMatch from './component/cardmatch/Cardmatch'
 import Nav from './component/nav/nav'
+import { ethers } from "ethers";
 
 export default function Home() {
+
+  const sports = [
+    {
+      id: 1,
+      matchName: "Arsenal vs Sunderland",
+      homePercentage: "100%",
+      drawPercentage: "25%",
+      awayPercentage: "30%"
+    },
+    {
+      id: 1,
+      matchName: "Arsenal vs Sunderland",
+      homePercentage: "45%",
+      drawPercentage: "25%",
+      awayPercentage: "30%"
+    },
+    {
+      id: 1,
+      matchName: "Arsenal vs Sunderland",
+      homePercentage: "45%",
+      drawPercentage: "25%",
+      awayPercentage: "30%"
+    },
+    {
+      id: 1,
+      matchName: "Arsenal vs Sunderland",
+      homePercentage: "45%",
+      drawPercentage: "25%",
+      awayPercentage: "30%"
+    },
+    {
+      id: 1,
+      matchName: "Arsenal vs Sunderland",
+      homePercentage: "45%",
+      drawPercentage: "25%",
+      awayPercentage: "30%"
+    },
+    {
+      id: 1,
+      matchName: "Arsenal vs Sunderland",
+      homePercentage: "45%",
+      drawPercentage: "25%",
+      awayPercentage: "30%"
+    },
+    {
+      id: 1,
+      matchName: "Arsenal vs Sunderland",
+      homePercentage: "45%",
+      drawPercentage: "25%",
+      awayPercentage: "30%"
+    },
+    {
+      id: 1,
+      matchName: "Arsenal vs Sunderland",
+      homePercentage: "45%",
+      drawPercentage: "25%",
+      awayPercentage: "30%"
+    },
+    {
+      id: 1,
+      matchName: "Arsenal vs Sunderland",
+      homePercentage: "45%",
+      drawPercentage: "25%",
+      awayPercentage: "30%"
+    }
+  ]
+
   return (
     <div>
       <div>
         <Nav />
       </div>
 
-      <div class="flex">
-        <div class="w-1/5 bg-gray-500 h-12"></div>
-        <div class="w-3/5 bg-gray-400 h-12"></div>
-        <div class="w-1/5 bg-gray-500 h-12"></div>
+      <div className="flex w-full flex-wrap flex-row">
+        {sports.map((sport) => (
+          <CardMatch key={sport.id} sport={sport} />
+        ))}
       </div>
-
-      <CardMatch/>
     </div>
   )
 }
